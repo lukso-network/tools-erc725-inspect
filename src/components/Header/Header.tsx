98aeab21949c0f1fbdcf543af5d1b13277ab6e6b
Hugo Masclet <git@hugom.xyz>
2021-09-25 21:49:32 +0200

    Add LIP in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header: React.FC = () => {
   return (
     <nav className="navbar is-light">
-      <div className="navbar-menu">
+      <div className="navbar-menu is-active">
         <div className="navbar-start">
           <a className="navbar-item" href="/">
             ERC725 Inspect 📝🔍
@@ -14,10 +14,21 @@ const Header: React.FC = () => {
           <div className="navbar-item">
             <div className="buttons">
               <a
+                href="https://github.com/lukso-network/LIPs/tree/master/LSPs"
+                className="button is-light"
+              >
+                LUKSO LIP
+              </a>
+            </div>
+          </div>
+
+          <div className="navbar-item">
+            <div className="buttons">
+              <a
                 href="https://docs.lukso.tech/networks/l14-testnet"
                 className="button is-light"
               >
-                Network: LUKSO l14
+                Network: LUKSO L14
               </a>
             </div>
           </div>
