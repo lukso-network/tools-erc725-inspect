13f6c498115d9898717755ddabff26d7e10ef231
Hugo Masclet <git@hugom.xyz>
2021-09-25 21:33:25 +0200

    Improve display of data results

--- src/components/DataKeysTable/DataKeysTable.tsx
+++ src/components/DataKeysTable/DataKeysTable.tsx
@@ -54,54 +54,65 @@ const DataKeysTable: React.FC<Props> = ({ address, isErc725Y }) => {
         const keyInfo = explainErc725YKey(data.key);
 
         return (
-          <div key={data.key} className="py-5">
-            <h4 className="title is-4">{keyInfo.name}</h4>
+          <div key={data.key} className="content py-5">
+            <h4 className="title is-4">
+              {keyInfo.name}{' '}
+              <span className="tag is-link is-light">{keyInfo.keyType}</span>
+            </h4>
             <ul>
               <li>
-                <span className="tag is-link is-light">{keyInfo.keyType}</span>
+                Key: <code>{keyInfo.key}</code>
               </li>
               <li>
-                <code>{keyInfo.key}</code>
+                Raw value:{' '}
+                <code style={{ lineBreak: 'anywhere' }}>{data.value}</code>
               </li>
-              <li>{keyInfo.valueType}</li>
               <li>
-                Raw value: <code>{data.value}</code>{' '}
-                {keyInfo.valueType === 'address' && (
-                  <div className="buttons are-small">
-                    <a
-                      className="button is-primary is-light"
-                      target="_blank"
-                      rel="noreferrer"
-                      href={`https://universalprofile.cloud/${data.value}`}
-                    >
-                      View UP as Profile
-                    </a>
-                    <a
-                      className="button is-info is-light"
-                      target="_blank"
-                      rel="noreferrer"
-                      href={`https://universalprofile.cloud/asset/${data.value}`}
-                    >
-                      View UP as Asset
-                    </a>
-                    <a
-                      className="button is-link is-light"
-                      target="_blank"
-                      rel="noreferrer"
-                      href={`https://blockscout.com/lukso/l14/address/${data.value}`}
-                    >
-                      Blockscout
-                    </a>
-                    <a
-                      className="button is-link is-light"
-                      href={`/?address=${data.value}`}
-                    >
-                      ERC725 Inspect
-                    </a>
-                  </div>
+                Decoded value{' '}
+                <span className="tag is-link is-light">
+                  {keyInfo.valueType}
+                </span>
+                :{' '}
+                {keyInfo.valueType === 'address' ? (
+                  <>
+                    <code>{data.value}</code>
+                    <div className="buttons are-small pt-2">
+                      <a
+                        className="button is-primary is-light"
+                        target="_blank"
+                        rel="noreferrer"
+                        href={`https://universalprofile.cloud/${data.value}`}
+                      >
+                        View on UP as Profile 🧑‍🎤
+                      </a>
+                      <a
+                        className="button is-info is-light"
+                        target="_blank"
+                        rel="noreferrer"
+                        href={`https://universalprofile.cloud/asset/${data.value}`}
+                      >
+                        View on UP as Asset 👗
+                      </a>
+                      <a
+                        className="button is-link is-light"
+                        target="_blank"
+                        rel="noreferrer"
+                        href={`https://blockscout.com/lukso/l14/address/${data.value}`}
+                      >
+                        View on Blockscout ⛓
+                      </a>
+                      <a
+                        className="button is-link is-light"
+                        href={`/?address=${data.value}`}
+                      >
+                        ERC725 Inspect 🔍
+                      </a>
+                    </div>
+                  </>
+                ) : (
+                  'TODO'
                 )}
               </li>
-              <li>Decoded value: TODO</li>
             </ul>
           </div>
         );
