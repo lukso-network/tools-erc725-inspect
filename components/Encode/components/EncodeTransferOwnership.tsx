a554e03384325cb6cb6713b46a7a5b623ef29328
Callum <callum@lukso.io>
2022-01-12 15:23:47 +0000

    Make TransferOwnership encoding more responsive

--- components/Encode/components/EncodeTransferOwnership.tsx
+++ components/Encode/components/EncodeTransferOwnership.tsx
@@ -4,6 +4,7 @@ import Web3 from "web3";
 import EncodedPayload from "./EncodedPayload";
 import ERC725Account from "../../../abis/ERC725Account.json"
 import ErrorMessage from "../../ErrorMessage";
+import { Contract } from "web3-eth-contract";
 
 interface Props {
     web3: Web3;
@@ -14,14 +15,22 @@ const EncodeTransferOwnership: React.FC<Props> = ({ web3 }) => {
     const [encodedPayload, setEncodedPayload] = useState("");
     const [encodingError, setEncodingError] = useState({ isError: false, message: "" });
 
-
+    let erc725Account: Contract;
     const encodeABI = () => {
-        const erc725Account = new web3.eth.Contract(ERC725Account.abi as any);
+
+    }
+
+    const handleChange = (value: string) => {
+        setNewOwner(value);
+
+        if (!erc725Account) {
+            erc725Account = new web3.eth.Contract(ERC725Account.abi as any);
+        }
 
         try {
             setEncodedPayload(
                 erc725Account.methods
-                    .transferOwnership(newOwner)
+                    .transferOwnership(value)
                     .encodeABI()
             );
 
@@ -38,19 +47,11 @@ const EncodeTransferOwnership: React.FC<Props> = ({ web3 }) => {
                 label="New owner"
                 value={newOwner}
                 fullWidth
-                onChange={(event) => setNewOwner(event.target.value)}
+                onChange={(event) => handleChange(event.target.value)}
             />
             <div
                 style={{ height: 300, width: "100%", marginBottom: 10, marginTop: 10 }}
             >
-                <Button
-                    variant="contained"
-                    color="primary"
-                    size="large"
-                    onClick={encodeABI}
-                >
-                    Encode ABI
-                </Button>
 
                 {encodedPayload ? <EncodedPayload encodedPayload={encodedPayload} /> : null}
                 {encodingError.isError ? <ErrorMessage header='ABI Encoding Error!' message={encodingError.message} /> : null}
