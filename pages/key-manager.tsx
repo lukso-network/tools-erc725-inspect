f936db06168440336580b1711d1032ac4e090124
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2022-06-22 16:34:26 +0100

    feat: add section in KeyManager to retrieve nonce

--- pages/key-manager.tsx
+++ pages/key-manager.tsx
@@ -2,6 +2,7 @@ import ERC725 from '@erc725/erc725.js';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import { useState } from 'react';
+import useWeb3 from '../hooks/useWeb3';
 
 const KeyManager: NextPage = () => {
   const [encodedPermissions, setEncodedPermissions] = useState(
@@ -140,7 +141,170 @@ const KeyManager: NextPage = () => {
           </div>
         </div>
       </div>
+
+      <Nonce />
     </>
+  );
+};
+
+const Nonce = () => {
+  const web3 = useWeb3();
+
+  const [keyManagerAddress, setKeyManagerAddress] = useState('');
+  const [callerAddress, setCallerAddress] = useState('');
+  const [channelId, setChannelId] = useState('');
+  const [nonce, setNonce] = useState('');
+
+  const [showNonce, setShowNonce] = useState(false);
+  const [error, showError] = useState(false);
+
+  const getNonceFromKeyManager = async () => {
+    if (!web3) return;
+
+    const keyManagerInstance = new web3.eth.Contract(
+      [
+        {
+          inputs: [
+            {
+              internalType: 'address',
+              name: '_from',
+              type: 'address',
+            },
+            {
+              internalType: 'uint256',
+              name: '_channel',
+              type: 'uint256',
+            },
+          ],
+          name: 'getNonce',
+          outputs: [
+            {
+              internalType: 'uint256',
+              name: '',
+              type: 'uint256',
+            },
+          ],
+          stateMutability: 'view',
+          type: 'function',
+        },
+        {
+          inputs: [
+            {
+              internalType: 'bytes4',
+              name: 'interfaceId',
+              type: 'bytes4',
+            },
+          ],
+          name: 'supportsInterface',
+          outputs: [
+            {
+              internalType: 'bool',
+              name: '',
+              type: 'bool',
+            },
+          ],
+          stateMutability: 'view',
+          type: 'function',
+        },
+      ],
+      web3.utils.toChecksumAddress(keyManagerAddress),
+    );
+
+    const isKeyManagerV05 = await keyManagerInstance.methods
+      .supportsInterface('0x6f4df48b')
+      .call();
+
+    const isKeyManagerV06 = await keyManagerInstance.methods
+      .supportsInterface('0xc403d48f')
+      .call();
+
+    if (isKeyManagerV05 || isKeyManagerV06) {
+      const result: any = await keyManagerInstance.methods
+        .getNonce(callerAddress, channelId)
+        .call();
+
+      setNonce(result);
+      setShowNonce(true);
+      showError(false);
+    } else {
+      setShowNonce(false);
+      showError(true);
+    }
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1 className="title is-2">Nonce</h1>
+      <p>
+        Retrieve the nonce of an <code>address</code> for a specific Key
+        Manager.
+      </p>
+      <div className="columns mt-2">
+        <div className="column">
+          <div className="field">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="text"
+                placeholder="Key Manager address"
+                onChange={(e) => {
+                  setKeyManagerAddress(e.target.value);
+                }}
+              />
+              <span className="icon is-small is-left">🔐</span>
+            </p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="field">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="text"
+                placeholder="Caller address"
+                onChange={(e) => setCallerAddress(e.target.value)}
+              />
+              <span className="icon is-small is-left">➡️ </span>
+            </p>
+          </div>
+        </div>
+        <div className="column">
+          <div className="field">
+            <p className="control has-icons-left">
+              <input
+                className="input"
+                type="text"
+                placeholder="Channel ID"
+                onChange={(e) => setChannelId(e.target.value)}
+              />
+              <span className="icon is-small is-left">🚪</span>
+            </p>
+          </div>
+        </div>
+        <div className="column">
+          <button
+            className="button is-success"
+            onClick={async () => {
+              await getNonceFromKeyManager();
+            }}
+          >
+            Get Nonce
+          </button>
+        </div>
+      </div>
+      <div
+        className="notification is-danger is-light"
+        style={{ display: error ? 'block' : 'none' }}
+      >
+        address <code>{keyManagerAddress}</code> is not a KeyManager
+      </div>
+      <div
+        className="notification is-success is-light"
+        style={{ display: showNonce ? 'block' : 'none' }}
+      >
+        Nonce = <code>{nonce}</code>
+      </div>
+    </div>
   );
 };
 
