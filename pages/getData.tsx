30600d86686900b37dd05b112ba6d4f935803e8c
b00ste <b00ste.lyx@gmail.com>
2022-09-19 13:50:23 +0000

    save `0x4b80742de2bf82acb3630000` as a constant

--- pages/getData.tsx
+++ pages/getData.tsx
@@ -22,6 +22,10 @@ const dataKeyList = [
   ...LSP6DataKeys.map((key) => ({ name: key.name, key: key.key, icon: '🔑' })),
   ...LSP9DataKeys.map((key) => ({ name: key.name, key: key.key, icon: '🔒' })),
 ];
+const ADDDRESS_PERMISSIONS_PERMISSION_PREFIX = dataKeyList[17].key.substring(
+  0,
+  26,
+);
 
 const GetData: NextPage = () => {
   const [address, setAddress] = useState('');
@@ -213,7 +217,8 @@ const GetData: NextPage = () => {
                 </div>
               </article>
               <pre style={{ wordBreak: 'break-all', whiteSpace: 'pre-wrap' }}>
-                {dataKey.substring(0, 26) == '0x4b80742de2bf82acb3630000'
+                {dataKey.substring(0, 26) ==
+                ADDDRESS_PERMISSIONS_PERMISSION_PREFIX
                   ? JSON.stringify(
                       ERC725.decodePermissions(data[0]),
                       undefined,
