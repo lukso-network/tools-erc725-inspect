8a998c9ca9029da6df94112bb813b533dbd611b9
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-11-06 18:44:46 +0100

    feat: setup cards on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import packageJson from '../package.json';
+import CardContainer from '../components/HomeCards/CardContainer';
 
 const ERC725_JS_VERSION = packageJson.dependencies['@erc725/erc725.js'];
 const LSP_SMART_CONTRACTS_VERSION =
@@ -12,34 +13,81 @@ const Home: NextPage = () => {
       <Head>
         <title>ERC725 Tools</title>
       </Head>
-      <div className="container">
+      <div className="home-container container">
         <div className="content">
-          <h1 className="title is-1">🛠 ERC725 Tools</h1>
+          <h1 className="title is-1">Welcome to ERC725 Tools</h1>
           <p>
             This website provides debugging tools to interact with ERC725 smart
             contracts.
           </p>
-          <h3 className="title is-3">Built with</h3>
-          <ul>
-            <li>
-              <a
-                className="has-text-link"
-                href="https://docs.lukso.tech/tools/erc725js/getting-started"
-              >
-                erc725.js
-              </a>
-              &nbsp;(version: {ERC725_JS_VERSION})
-            </li>
-            <li>
-              <a
-                className="has-text-link"
-                href="https://docs.lukso.tech/tools/lsp-smart-contracts/getting-started"
-              >
-                lsp-smart-contracts{' '}
-              </a>
-              (version: {LSP_SMART_CONTRACTS_VERSION})
-            </li>
-          </ul>
+          <CardContainer
+            cardData={[
+              {
+                title: '🔎 Inspector',
+                description:
+                  'Check the data keys and interfaces of smart contract.',
+                link: '/inspector',
+                isExternal: false,
+              },
+              {
+                title: '💽 Data Fetcher',
+                description:
+                  'Retrieve the contents of the smart contract data keys.',
+                link: '/data-fetcher',
+              },
+              {
+                title: '🔐 Key Manager',
+                description:
+                  'Encode and decode smart contract permissions of the key manager.',
+                link: '/key-manager',
+              },
+              {
+                title: '📜 ABI Encoder',
+                description:
+                  'Encode and decode smart contract transaction data.',
+                link: '/abi-encoder',
+              },
+              {
+                title: '📖 LSP2 Encoder ',
+                description:
+                  'Encode and decode storage information based on the JSON schema.',
+                link: 'lsp2-encoder',
+              },
+            ]}
+          ></CardContainer>
+          <h3 className="title is-4">External Developer Resources</h3>
+          <p>
+            Start integrating ERC725 smart contracts into your own projects by
+            diving into the following apps and libraries:
+          </p>
+          <CardContainer
+            cardData={[
+              {
+                title: 'Test dApp',
+                description:
+                  'The test dApp demonstrates the deployment and interaction with Universal Profiles in the browser environment.',
+                link: 'https://up-test-dapp.lukso.tech/',
+                isExternal: true,
+                version: '1.0.0',
+              },
+              {
+                title: 'erc-725.js',
+                description:
+                  'The ERC725 library allows seamless interaction of smart contract schemas and contracts to simpify integration.',
+                link: 'https://docs.lukso.tech/tools/erc725js/getting-started',
+                isExternal: true,
+                version: ERC725_JS_VERSION,
+              },
+              {
+                title: 'lsp-smart-contracts',
+                description:
+                  'The LSP library includes Solidity code, JSON ABIs, and constants, to streamline your smart contract development.',
+                link: 'https://docs.lukso.tech/tools/lsp-smart-contracts/getting-started',
+                isExternal: true,
+                version: LSP_SMART_CONTRACTS_VERSION,
+              },
+            ]}
+          ></CardContainer>
         </div>
       </div>
     </>
