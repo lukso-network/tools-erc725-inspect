9c4b5c0f6d303b9ea9799a1b0ede90045ec343d7
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-11-08 19:08:18 +0100

    feat: remove mui package and frame

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,16 @@
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
-import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
 import NavBar from '../components/NavBar';
 import NetworksProvider from '../contexts/NetworksContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <NetworksProvider>
-      <ScopedCssBaseline>
-        <NavBar />
-        <section className="section">
-          <Component {...pageProps} />
-        </section>
-      </ScopedCssBaseline>
+      <NavBar />
+      <section className="section">
+        <Component {...pageProps} />
+      </section>
     </NetworksProvider>
   );
 }
