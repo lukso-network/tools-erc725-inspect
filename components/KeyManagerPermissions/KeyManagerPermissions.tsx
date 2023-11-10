2c35be3b4b127bed71ea51c9b071a86daf988d76
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2023-11-10 11:43:06 +0100

    feat: add links and headings to key manager

--- components/KeyManagerPermissions/KeyManagerPermissions.tsx
+++ components/KeyManagerPermissions/KeyManagerPermissions.tsx
@@ -39,20 +39,57 @@ const KeyManagerPermissions: React.FC = () => {
       <h2 className="title is-2">Permissions</h2>
       <article className="message is-info">
         <div className="message-body">
-          This tool will encode/decode permissions according to{' '}
-          <a href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager">
-            LSP6 KeyManager Standard
-          </a>{' '}
-          smart contract and attempt to match them with their{' '}
-          <a href="https://github.com/lukso-network/LIPs/blob/main/LSPs/LSP-2-ERC725YJSONSchema.md">
+          This tool will encode and decode
+          <a
+            href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager/#address-permissions"
+            target="_blank"
+            rel="noreferrer"
+            className="mr-1 ml-1"
+          >
+            permissions
+          </a>
+          based on the
+          <a
+            href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager"
+            target="_blank"
+            rel="noreferrer"
+            className="mr-1 ml-1"
+          >
+            LSP6 KeyManager
+          </a>
+          standardization to match them with their
+          <a
+            href="https://github.com/lukso-network/LIPs/blob/main/LSPs/LSP-2-ERC725YJSONSchema.md"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-1"
+          >
             LSP2 ERC725YJSONSchema
           </a>
-          .<br />
-          The erc725.js lib provides a{' '}
-          <a href="https://docs.lukso.tech/tools/erc725js/classes/ERC725#encodepermissions">
-            decodePermissions
-          </a>{' '}
-          method to decode the permissions.
+          .
+        </div>
+      </article>
+      <article className="message">
+        <div className="message-body">
+          Its using the
+          <a
+            href="https://docs.lukso.tech/tools/erc725js/classes/ERC725#encodepermissions"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-1 mr-1"
+          >
+            encodePermissions
+          </a>
+          function of the
+          <a
+            href="https://www.npmjs.com/package/@erc725/erc725.js"
+            target="_blank"
+            rel="noreferrer"
+            className="ml-1 mr-1"
+          >
+            erc725.js
+          </a>
+          library.
         </div>
       </article>
 
@@ -69,18 +106,51 @@ const KeyManagerPermissions: React.FC = () => {
       </div>
       <div className="columns">
         <div className="column">
+          <h2 className="mb-2 subtitle is-5">
+            Manage Contract Ownership
+            <a
+              href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager/#permissions"
+              target="_blank"
+              rel="noreferrer"
+              className="ml-2 has-text-info-dark"
+            >
+              ↗
+            </a>
+          </h2>
           <PermissionsBtns
             permissions={['CHANGEOWNER', 'ADDCONTROLLER', 'EDITPERMISSIONS']}
             color={'is-orange-dark'}
             decodedPermissions={decodedPermissions}
             handlePermissionClick={handlePermissionClick}
           />
+          <h2 className="mb-2 subtitle is-5">
+            Manage Extensions
+            <a
+              href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager/#permissions"
+              target="_blank"
+              rel="noreferrer"
+              className="ml-2 has-text-info-dark"
+            >
+              ↗
+            </a>
+          </h2>
           <PermissionsBtns
             permissions={['ADDEXTENSIONS', 'CHANGEEXTENSIONS']}
             color={'is-orange'}
             decodedPermissions={decodedPermissions}
             handlePermissionClick={handlePermissionClick}
           />
+          <h2 className="mb-2 subtitle is-5">
+            Manage Universal Receivers
+            <a
+              href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager/#permissions"
+              target="_blank"
+              rel="noreferrer"
+              className="ml-2 has-text-info-dark"
+            >
+              ↗
+            </a>
+          </h2>
           <PermissionsBtns
             permissions={[
               'ADDUNIVERSALRECEIVERDELEGATE',
@@ -90,12 +160,34 @@ const KeyManagerPermissions: React.FC = () => {
             decodedPermissions={decodedPermissions}
             handlePermissionClick={handlePermissionClick}
           />
+          <h2 className="mb-2 subtitle is-5">
+            Manage Payload Execution
+            <a
+              href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager/#permissions"
+              target="_blank"
+              rel="noreferrer"
+              className="ml-2 has-text-info-dark"
+            >
+              ↗
+            </a>
+          </h2>
           <PermissionsBtns
             permissions={['REENTRANCY']}
             color={'is-danger'}
             decodedPermissions={decodedPermissions}
             handlePermissionClick={handlePermissionClick}
           />
+          <h2 className="mb-2 subtitle is-5">
+            Manage Contract Interactions
+            <a
+              href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager/#permissions"
+              target="_blank"
+              rel="noreferrer"
+              className="ml-2 has-text-info-dark"
+            >
+              ↗
+            </a>
+          </h2>
           <PermissionsBtns
             permissions={[
               'SUPER_TRANSFERVALUE',
@@ -112,12 +204,34 @@ const KeyManagerPermissions: React.FC = () => {
             decodedPermissions={decodedPermissions}
             handlePermissionClick={handlePermissionClick}
           />
+          <h2 className="mb-2 subtitle is-5">
+            Manage Contract Storage
+            <a
+              href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager/#permissions"
+              target="_blank"
+              rel="noreferrer"
+              className="ml-2 has-text-info-dark"
+            >
+              ↗
+            </a>
+          </h2>
           <PermissionsBtns
             permissions={['SUPER_SETDATA', 'SETDATA']}
             color={'is-success'}
             decodedPermissions={decodedPermissions}
             handlePermissionClick={handlePermissionClick}
           />
+          <h2 className="mb-2 subtitle is-5">
+            Manage Signing Verification
+            <a
+              href="https://docs.lukso.tech/standards/universal-profile/lsp6-key-manager/#permissions"
+              target="_blank"
+              rel="noreferrer"
+              className="ml-2 has-text-info-dark"
+            >
+              ↗
+            </a>
+          </h2>
           <PermissionsBtns
             permissions={['ENCRYPT', 'DECRYPT', 'SIGN']}
             color={'is-purple'}
