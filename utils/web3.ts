03a5e286b7912fd9bcd164003e4c1064c854dfbd
Jean Cvllr <31145285+CJ42@users.noreply.github.com>
2024-04-17 20:40:33 +0100

    Decode metadata of tokens deployed with old + new interface ID (#103)
    * build: upgrade lsp-smart-contracts to 0.15.0
    
    * feat: check for old + new interface IDs for LSP7 & 8
    
    * refactor: cleanup code into a single function
    
    * refactor: use multicall to check for multiple interfaces in 2 RPC calls

--- utils/web3.ts
+++ utils/web3.ts
@@ -3,7 +3,23 @@
  */
 import Web3 from 'web3';
 import { INTERFACE_IDS } from '@lukso/lsp-smart-contracts';
-import { eip165ABI, getDataBatchABI, getDataABI } from '../constants';
+import {
+  INTERFACE_ID_LSP7,
+  INTERFACE_ID_LSP7_PREVIOUS,
+} from '@lukso/lsp7-contracts';
+
+import {
+  INTERFACE_ID_LSP8_PREVIOUS,
+  INTERFACE_ID_LSP8,
+} from '@lukso/lsp8-contracts';
+import {
+  eip165ABI,
+  getDataBatchABI,
+  getDataABI,
+  VersionABI,
+  MULTICALL_CONTRACT_ADDRESS,
+  aggregateABI,
+} from '../constants';
 import { AbiItem } from 'web3-utils';
 
 export const getDataBatch = async (
@@ -28,7 +44,7 @@ export const getData = async (
   key: string,
   web3: Web3,
 ): Promise<string | null> => {
-  const Contract = new web3.eth.Contract(getDataABI as AbiItem[], address);
+  const Contract = new web3.eth.Contract(getDataABI, address);
 
   let data: string | null = null;
   try {
@@ -41,124 +57,108 @@ export const getData = async (
 };
 
 export const checkInterface = async (address: string, web3: Web3) => {
-  const Contract = new web3.eth.Contract(eip165ABI as any, address);
+  // aggregate multiple supportsInterface calls in a batch Multicall for efficiency
+  const supportsContractInterface = await checkSupportedInterfaces(
+    address,
+    [
+      INTERFACE_IDS.ERC725X,
+      INTERFACE_IDS.ERC725Y,
+      INTERFACE_IDS.ERC1271,
+      INTERFACE_IDS.LSP0ERC725Account,
+      INTERFACE_IDS.LSP1UniversalReceiver,
+      INTERFACE_IDS.LSP6KeyManager,
+      INTERFACE_IDS.LSP9Vault,
+    ],
+    web3,
+  );
 
-  let isErc725X = false;
-  try {
-    isErc725X = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.ERC725X)
-      .call();
-  } catch (err: any) {
-    console.log(err.message);
-  }
-
-  let isErc725Y = false;
-  try {
-    isErc725Y = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.ERC725Y)
-      .call();
-  } catch (err: any) {
-    console.warn(err.message);
-  }
-
-  let isErc1271 = false;
-  try {
-    isErc1271 = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.ERC1271)
-      .call();
-  } catch (err: any) {
-    console.warn(err.message);
-  }
-
-  let isLsp0Erc725Account = false;
-  try {
-    isLsp0Erc725Account = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.LSP0ERC725Account)
-      .call();
-  } catch (err: any) {
-    console.warn(err.message);
-  }
-
-  let isLsp1UniversalReceiver = false;
-  try {
-    isLsp1UniversalReceiver = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.LSP1UniversalReceiver)
-      .call();
-  } catch (err: any) {
-    console.warn(err.message);
-  }
-
-  let isLsp6KeyManager = false;
-  try {
-    isLsp6KeyManager = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.LSP6KeyManager)
-      .call();
-  } catch (err: any) {
-    console.warn(err.message);
-  }
-
-  let isLsp7DigitalAsset = false;
-  try {
-    isLsp7DigitalAsset = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.LSP7DigitalAsset)
-      .call();
-  } catch (err: any) {
-    console.warn(err.message);
-  }
-
-  let isLsp8IdentifiableDigitalAsset = false;
-  try {
-    isLsp8IdentifiableDigitalAsset = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.LSP8IdentifiableDigitalAsset)
-      .call();
-  } catch (err: any) {
-    console.warn(err.message);
-  }
-
-  let isLsp9Vault = false;
-  try {
-    isLsp9Vault = await Contract.methods
-      .supportsInterface(INTERFACE_IDS.LSP9Vault)
-      .call();
-  } catch (err: any) {
-    console.warn(err.message);
-  }
+  // digital assets need to be checked against multiple interface IDs
+  const supportsAssetInterface = await checkSupportedInterfaces(
+    address,
+    [
+      INTERFACE_ID_LSP7_PREVIOUS['v0.12.0'],
+      INTERFACE_ID_LSP7_PREVIOUS['v0.14.0'],
+      INTERFACE_ID_LSP7,
+      INTERFACE_ID_LSP8_PREVIOUS['v0.12.0'],
+      INTERFACE_ID_LSP8_PREVIOUS['v0.14.0'],
+      INTERFACE_ID_LSP8,
+    ],
+    web3,
+  );
 
   return {
-    isErc725X,
-    isErc725Y,
-    isErc1271,
-    isLsp0Erc725Account,
-    isLsp1UniversalReceiver,
-    isLsp6KeyManager,
-    isLsp7DigitalAsset,
-    isLsp8IdentifiableDigitalAsset,
-    isLsp9Vault,
+    isErc725X: supportsContractInterface[0],
+    isErc725Y: supportsContractInterface[1],
+    isErc1271: supportsContractInterface[2],
+    isLsp0Erc725Account: supportsContractInterface[3],
+    isLsp1UniversalReceiver: supportsContractInterface[4],
+    isLsp6KeyManager: supportsContractInterface[5],
+    isLsp7DigitalAsset:
+      supportsAssetInterface[0] ||
+      supportsAssetInterface[1] ||
+      supportsAssetInterface[2],
+    isLsp8IdentifiableDigitalAsset:
+      supportsAssetInterface[3] ||
+      supportsAssetInterface[4] ||
+      supportsAssetInterface[5],
+    isLsp9Vault: supportsContractInterface[6],
   };
 };
+
+export const aggregateCalls = async (
+  calls: [string, string][],
+  web3: Web3,
+): Promise<string[]> => {
+  try {
+    const multiCallContract = new web3.eth.Contract(
+      aggregateABI,
+      MULTICALL_CONTRACT_ADDRESS,
+    );
+
+    const result = await multiCallContract.methods.aggregate(calls).call();
+    return result.returnData;
+  } catch (error) {
+    console.warn('could not aggregate results: ', error);
+    return ['', ''];
+  }
+};
+
+async function checkSupportedInterfaces(
+  assetAddress: string,
+  interfaceIds: string[],
+  web3: Web3,
+): Promise<boolean[]> {
+  const eip165Instance = new web3.eth.Contract(eip165ABI);
+
+  const supportsInterfaceCalls: [string, string][] = interfaceIds.map(
+    (interfaceId) => {
+      const result: [string, string] = [
+        assetAddress,
+        eip165Instance.methods.supportsInterface(interfaceId).encodeABI(),
+      ];
+      return result;
+    },
+  );
+  const response = await aggregateCalls(supportsInterfaceCalls, web3);
+
+  return response.map((entry) => {
+    try {
+      return web3.eth.abi.decodeParameter('bool', entry) as unknown as boolean;
+    } catch (decodingError) {
+      console.warn(
+        'Could not decode `supportsInterface` return data from aggregate call as `boolean`: ',
+        decodingError,
+      );
+      return false;
+    }
+  });
+}
 
 export const getVersion = async (
   address: string,
   web3: Web3,
 ): Promise<string> => {
-  const Contract = new web3.eth.Contract(
-    [
-      {
-        inputs: [],
-        name: 'VERSION',
-        outputs: [
-          {
-            internalType: 'string',
-            name: '',
-            type: 'string',
-          },
-        ],
-        stateMutability: 'view',
-        type: 'function',
-      },
-    ],
-    address,
-  );
+  const Contract = new web3.eth.Contract(VersionABI, address);
 
   try {
     const result = await Contract.methods.VERSION().call();
