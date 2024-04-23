606bb4af3a8e8ba55868bbeed31662160f47b079
Felix Hildebrandt <felix.hildebrandt@gmx.net>
2024-04-23 19:28:40 +0200

    set up global network parameter

--- contexts/NetworksContext.tsx
+++ contexts/NetworksContext.tsx
@@ -1,11 +1,30 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useCallback, useEffect } from 'react';
 import { RPC_URL } from '../globals';
 import { NetworkName } from '../types/network';
+import { useRouter } from 'next/router';
 export interface INetwork {
   name: NetworkName;
   rpc: string;
   imgUrl?: string;
 }
+
+const luksoChains: INetwork[] = [
+  {
+    name: NetworkName.MAINNET,
+    rpc: RPC_URL[NetworkName.MAINNET],
+    imgUrl: '/lukso.png',
+  },
+  {
+    name: NetworkName.TESTNET,
+    rpc: RPC_URL[NetworkName.TESTNET],
+    imgUrl: '/lukso.png',
+  },
+  {
+    name: NetworkName.LOCALHOST,
+    rpc: RPC_URL[NetworkName.LOCALHOST],
+    imgUrl: '/lukso.png',
+  },
+];
 
 export interface INetworksContext {
   network: INetwork;
@@ -17,13 +36,53 @@ export const NetworkContext = createContext<INetworksContext>({
   setNetwork: () => null,
 });
 
-const NetworksProvider = ({ children }: { children: React.ReactNode }) => {
-  const [network, setNetwork] = useState<INetwork>({
-    // Default Network
-    name: NetworkName.MAINNET,
-    rpc: RPC_URL[NetworkName.MAINNET],
-    imgUrl: '/lukso.png',
-  });
+const NetworksProvider = ({ children }) => {
+  const router = useRouter();
+
+  // Get network from URL or switch to default chain
+  const getNetworkFromUrlOrDefault = useCallback(() => {
+    let networkParam = router.query.network;
+    if (Array.isArray(networkParam)) {
+      networkParam = networkParam[0];
+    }
+    return (
+      luksoChains.find(
+        (network) => network.name.toLowerCase() === networkParam?.toLowerCase(),
+      ) || luksoChains[0]
+    );
+  }, [router.query.network]);
+
+  // Initialize state based on network
+  const [network, setNetwork] = useState(getNetworkFromUrlOrDefault);
+
+  const updateUrlWithNetwork = useCallback(
+    (networkName) => {
+      if (typeof window !== 'undefined') {
+        const queryParams = new URLSearchParams(window.location.search);
+        queryParams.set('network', networkName);
+        const updatedUrl = `${router.pathname}?${queryParams.toString()}`;
+        router.replace(updatedUrl, undefined, { shallow: true });
+      }
+    },
+    [router],
+  );
+
+  useEffect(() => {
+    if (!router.isReady) return;
+
+    const initializedNetwork = getNetworkFromUrlOrDefault();
+    setNetwork(initializedNetwork);
+
+    // Update the URL with the network parameter if missing
+    if (!router.query.network) {
+      updateUrlWithNetwork(initializedNetwork.name.toLowerCase());
+    }
+  }, [
+    router.isReady,
+    router.query.network,
+    getNetworkFromUrlOrDefault,
+    updateUrlWithNetwork,
+  ]);
 
   return (
     <NetworkContext.Provider value={{ network, setNetwork }}>
