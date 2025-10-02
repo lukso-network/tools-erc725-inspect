6663e138b6df70a7c689d82b9d1b415656e7e70c
CJ42 <cavallerajean@gmail.com>
2025-10-02 16:09:32 +0100

    refactor: improve styling of address buttons cta to redirect to ue.io, blockscout or erc725-inspect (including redirect link)

--- components/AddressButtons/AddressButtons.tsx
+++ components/AddressButtons/AddressButtons.tsx
@@ -8,48 +8,76 @@ import { NetworkContext } from '@/contexts/NetworksContext';
 interface Props {
   address: string;
   showInspectButton?: boolean;
+  standards?: {
+    isLSP0ERC725Account: boolean;
+    isLSP7DigitalAsset: boolean;
+    isLSP8IdentifiableDigitalAsset: boolean;
+  } | null;
 }
 
 const AddressButtons: React.FC<Props> = ({
   address,
   showInspectButton = true,
+  standards,
 }) => {
   const { network } = useContext(NetworkContext);
 
   const networkType = network.name.toLocaleLowerCase();
 
+  const universalEverythingURL =
+    standards && standards.isLSP7DigitalAsset
+      ? `https://universaleverything.io/asset/${address}?network=${networkType}`
+      : `https://universaleverything.io/${address}?network=${networkType}`;
+
   return (
-    <div className="buttons is-centered are-small pt-2">
-      <a
-        className="button is-success is-normal"
+    <div className="buttons are-small flex is-flex-direction-column is-align-items-flex-start">
+      {standards && (
+        <a
+          className="button is-normal"
+          target="_blank"
+          rel="noreferrer"
+          href={universalEverythingURL}
+        >
+          <span className="icon is-small p-1">
+            <img
+              src="/universaleverything-logo.png"
+              alt="UniversalEverything"
+            />
+          </span>
+          <span>View on UniversalEverything.io</span>
+        </a>
+      )}
+      {/* <a
+        className="button is-normal"
         target="_blank"
         rel="noreferrer"
-        href={`https://wallet.universalprofile.cloud/${address}?network=${networkType}`}
+        href={universalEverythingURL}
       >
-        View on UP as Profile 🧑‍🎤
-      </a>
+        <span className="icon is-small p-1">
+          <img src="/universaleverything-logo.png" alt="UniversalEverything" />
+        </span>
+        <span>View on UniversalEverything.io</span>
+      </a> */}
       <a
-        className="button is-success is-normal"
-        target="_blank"
-        rel="noreferrer"
-        href={`https://wallet.universalprofile.cloud/asset/${address}?network=${networkType}`}
-      >
-        View on UP as Asset 👗
-      </a>
-      <a
-        className="button is-success is-normal"
+        className="button is-normal is-info"
         target="_blank"
         rel="noreferrer"
         href={`https://explorer.execution.${networkType}.lukso.network/address/${address}`}
       >
-        View on Blockscout ⛓
+        <span className="icon is-small">
+          <img src="/blockscout-logo-white.svg" alt="Blockscout" />
+        </span>
+        <span>View on Blockscout</span>
       </a>
       {showInspectButton && (
         <a
-          className="button is-success is-normal"
+          className="button is-primary is-normal"
           href={`${window.location.href.split('?')[0]}?address=${address}`}
         >
-          ERC725 Inspect 🔍
+          <span className="icon is-small">
+            <img src="/inspect-icon.svg" alt="ERC725 Inspect" />
+          </span>
+          <span>Inspect with erc725-inspect</span>
         </a>
       )}
     </div>
--- components/DataKeyBox/DataKeyBox.tsx
+++ components/DataKeyBox/DataKeyBox.tsx
@@ -17,72 +17,89 @@ type DataKeyBoxProps = {
 };
 
 const DataKeyBox = ({ address, data }: DataKeyBoxProps) => {
-  return (
-    <div className="column is-full mt-4 dataKeyBox" key={data.key}>
-      <div className="content">
-        <div className="title is-4">
-          {data.schema.name in SchemaName ? (
-            <a
-              href={SCHEMA_DOCS_LINKS[data.schema.name]}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="home-link"
-            >
-              {data.schema.name} ↗️
-            </a>
-          ) : (
-            data.schema.name
-          )}
+  const {
+    value,
+    schema: { keyType, valueContent },
+  } = data;
 
-          <span className="tag is-small mx-2 is-info">
-            {data.schema.keyType}
-          </span>
-        </div>
-        <details className="has-background-link-light p-2 is-clickable m-4 is-size-6 has-text-weight-light">
-          <summary className="has-text-weight-semibold">
-            See LSP2 JSON Schema of <code>{data.schema.name}</code>
-          </summary>
-          <CodeEditor
-            sourceCode={JSON.stringify(data.schema, null, 4)}
-            readOnly={true}
-          />
-        </details>
-        <ul>
-          <li>
-            <strong>Key:</strong> <code>{data.schema.key}</code>
-          </li>
-          <li>
-            <strong>Raw value: </strong>
-            <span className="tag is-small mx-2 is-link is-light">
-              {data.schema.valueType}
+  const columnClass = keyType === 'Array' ? 'is-full' : 'is-two-thirds';
+
+  return (
+    <div className="columns is-multiline my-3 dataKeyBox" key={data.key}>
+      <div className={`column ${columnClass}`}>
+        <div className="content">
+          <div className="title is-4">
+            {data.schema.name in SchemaName ? (
+              <a
+                href={SCHEMA_DOCS_LINKS[data.schema.name]}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="home-link"
+              >
+                {data.schema.name} ↗️
+              </a>
+            ) : (
+              data.schema.name
+            )}
+
+            <span className="tag is-small mx-2 is-info">
+              {data.schema.keyType}
             </span>
-            <code>{data.value}</code>
-          </li>
-          <li>
-            <strong>Value Content: </strong>
-            <span className="tag is-small is-link is-light">
-              {data.schema.valueContent}
-            </span>
-          </li>
-          <li>
-            <strong>Decoded value: </strong>
-            <span className="my-3 mr-3">
-              <ValueTypeDecoder
-                address={address}
-                erc725JSONSchema={data.schema}
-                value={data.value}
-              />
-            </span>
-          </li>
-          {data.schema.keyType === 'MappingWithGrouping' && (
+          </div>
+          <details className="has-background-link-light p-2 is-clickable m-4 is-size-6 has-text-weight-light">
+            <summary className="has-text-weight-semibold">
+              See LSP2 JSON Schema of <code>{data.schema.name}</code>
+            </summary>
+            <CodeEditor
+              sourceCode={JSON.stringify(data.schema, null, 4)}
+              readOnly={true}
+            />
+          </details>
+          <ul>
             <li>
-              Mapped address: <code>0x{data.schema.name.split(':').pop()}</code>{' '}
-              <AddressButtons
-                address={`0x${data.schema.name.split(':').pop()}`}
-              />
+              <strong>Key:</strong> <code>{data.schema.key}</code>
             </li>
+            <li>
+              <strong>Raw value: </strong>
+              <span className="tag is-small mx-2 is-link is-light">
+                {data.schema.valueType}
+              </span>
+              <code>{data.value}</code>
+            </li>
+            <li>
+              <strong>Value Content: </strong>
+              <span className="tag is-small is-link is-light">
+                {data.schema.valueContent}
+              </span>
+            </li>
+            <li>
+              <strong>Decoded value: </strong>
+              <span className="my-3 mr-3">
+                <ValueTypeDecoder
+                  address={address}
+                  erc725JSONSchema={data.schema}
+                  value={data.value}
+                />
+              </span>
+            </li>
+            {data.schema.keyType === 'MappingWithGrouping' && (
+              <li>
+                Mapped address:{' '}
+                <code>0x{data.schema.name.split(':').pop()}</code>{' '}
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+      <div className="column">
+        {keyType !== 'Array' &&
+          value !== '0x' &&
+          valueContent === 'Address' && (
+            <AddressButtons
+              showInspectButton={true}
+              address={value as string}
+            />
           )}
-        </ul>
       </div>
     </div>
   );
