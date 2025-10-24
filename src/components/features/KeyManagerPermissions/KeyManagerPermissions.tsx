cc2437beb2cb140860f82777ce6b8d8af3e7c965
CJ42 <cavallerajean@gmail.com>
2025-10-24 21:15:22 +0100

    refactor: improve styling of Key Manager permission page

--- src/components/features/KeyManagerPermissions/KeyManagerPermissions.tsx
+++ src/components/features/KeyManagerPermissions/KeyManagerPermissions.tsx
@@ -5,6 +5,7 @@ import ERC725 from '@erc725/erc725.js';
 import PermissionsBtns from '@/components/ui/PermissionsBtns';
 import ToolInfos from '@/components/layout/ToolInfos';
 import { LSP_DOCS_URL } from '@/constants/links';
+import CodeEditor from '@/components/ui/CodeEditor';
 
 const KeyManagerPermissions: React.FC = () => {
   const initialEncodedPermissions =
@@ -39,7 +40,7 @@ const KeyManagerPermissions: React.FC = () => {
   };
 
   return (
-    <div className="container">
+    <div className="container container-key-manager">
       <h2 className="title is-2">Permissions</h2>
       <ToolInfos
         erc725jsMethod={['encodePermissions', 'decodePermissions']}
@@ -74,79 +75,147 @@ const KeyManagerPermissions: React.FC = () => {
         </div>
       </div>
       <div className="columns">
-        <div className="column is-half">
-          <h5 className="mb-2 title is-5">Contract Ownership</h5>
-          <PermissionsBtns
-            permissions={['CHANGEOWNER', 'ADDCONTROLLER', 'EDITPERMISSIONS']}
-            color={'is-orange-dark'}
-            decodedPermissions={decodedPermissions}
-            handlePermissionClick={handlePermissionClick}
-          />
-          <h5 className="mb-2 title is-5">Extensions</h5>
-          <PermissionsBtns
-            permissions={['ADDEXTENSIONS', 'CHANGEEXTENSIONS']}
-            color={'is-orange'}
-            decodedPermissions={decodedPermissions}
-            handlePermissionClick={handlePermissionClick}
-          />
-          <h5 className="mb-2 title is-5">Universal Receivers</h5>
-          <PermissionsBtns
-            permissions={[
-              'ADDUNIVERSALRECEIVERDELEGATE',
-              'CHANGEUNIVERSALRECEIVERDELEGATE',
-            ]}
-            color={'is-warning'}
-            decodedPermissions={decodedPermissions}
-            handlePermissionClick={handlePermissionClick}
-          />
-          <h5 className="mb-2 title is-5">Payload Execution</h5>
-          <PermissionsBtns
-            permissions={['REENTRANCY', 'ERC4337_PERMISSION']}
-            color={'is-danger'}
-            decodedPermissions={decodedPermissions}
-            handlePermissionClick={handlePermissionClick}
-          />
-          <h5 className="mb-2 title is-5">Contract Interactions</h5>
-          <PermissionsBtns
-            permissions={[
-              'SUPER_TRANSFERVALUE',
-              'TRANSFERVALUE',
-              'SUPER_CALL',
-              'CALL',
-              'SUPER_STATICCALL',
-              'STATICCALL',
-              'SUPER_DELEGATECALL',
-              'DELEGATECALL',
-              'DEPLOY',
-            ]}
-            color={'is-primary'}
-            decodedPermissions={decodedPermissions}
-            handlePermissionClick={handlePermissionClick}
-          />
-          <h5 className="mb-2 title is-5">Contract Storage</h5>
-          <PermissionsBtns
-            permissions={['SUPER_SETDATA', 'SETDATA']}
-            color={'is-success'}
-            decodedPermissions={decodedPermissions}
-            handlePermissionClick={handlePermissionClick}
-          />
-          <h5 className="mb-2 title is-5">Signing Verification</h5>
-          <PermissionsBtns
-            permissions={['ENCRYPT', 'DECRYPT', 'SIGN']}
-            color={'is-purple'}
-            decodedPermissions={decodedPermissions}
-            handlePermissionClick={handlePermissionClick}
-          />
-          <h5 className="mb-2 title is-5">Relay Transactions</h5>
-          <PermissionsBtns
-            permissions={['EXECUTE_RELAY_CALL']}
-            color={'is-info'}
-            decodedPermissions={decodedPermissions}
-            handlePermissionClick={handlePermissionClick}
-          />
+        <div className="column is-two-thirds">
+          <table className="table is-fullwidth mb-4">
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Permissions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <strong className="mb-2 ">Administration & Ownership</strong>
+                </td>
+                <td>
+                  <PermissionsBtns
+                    permissions={[
+                      'CHANGEOWNER',
+                      'ADDCONTROLLER',
+                      'EDITPERMISSIONS',
+                    ]}
+                    color={'is-red'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong className="mb-2 ">LSP17 Extensions</strong>
+                </td>
+                <td>
+                  {' '}
+                  <PermissionsBtns
+                    permissions={['ADDEXTENSIONS', 'CHANGEEXTENSIONS']}
+                    color={'is-extension'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong className="mb-2 ">
+                    LSP1 Universal Receivers Delegates
+                  </strong>
+                </td>
+                <td>
+                  {' '}
+                  <PermissionsBtns
+                    permissions={[
+                      'ADDUNIVERSALRECEIVERDELEGATE',
+                      'CHANGEUNIVERSALRECEIVERDELEGATE',
+                    ]}
+                    color={'is-fuschia'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong className="mb-2 ">Contract Interactions</strong>
+                </td>
+                <td>
+                  {' '}
+                  <PermissionsBtns
+                    permissions={[
+                      'SUPER_TRANSFERVALUE',
+                      'TRANSFERVALUE',
+                      'SUPER_CALL',
+                      'CALL',
+                      'SUPER_STATICCALL',
+                      'STATICCALL',
+                      'DEPLOY',
+                    ]}
+                    color={'is-yellow'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                  <PermissionsBtns
+                    permissions={['EXECUTE_RELAY_CALL']}
+                    color={'is-orange'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                  <PermissionsBtns
+                    permissions={[
+                      'SUPER_DELEGATECALL',
+                      'DELEGATECALL',
+                      'REENTRANCY',
+                    ]}
+                    color={'is-red'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                  <PermissionsBtns
+                    permissions={['ERC4337_PERMISSION']}
+                    color={'is-grey'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong className="mb-2 ">Metadata</strong>
+                </td>
+                <td>
+                  {' '}
+                  <PermissionsBtns
+                    permissions={['SUPER_SETDATA', 'SETDATA']}
+                    color={'is-blue'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <strong className="mb-2 ">Signing Verification</strong>
+                </td>
+                <td>
+                  {' '}
+                  <PermissionsBtns
+                    permissions={['SIGN', 'ENCRYPT', 'DECRYPT']}
+                    color={'is-purple'}
+                    decodedPermissions={decodedPermissions}
+                    handlePermissionClick={handlePermissionClick}
+                  />
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <div className="column is-half">
-          <pre>{JSON.stringify(decodedPermissions, undefined, 2)}</pre>
+        <div className="column is-one-third">
+          <div className="mb-4 p-4 has-background-light">
+            <CodeEditor
+              sourceCode={JSON.stringify(decodedPermissions, undefined, 2)}
+              readOnly
+            />
+          </div>
         </div>
       </div>
     </div>
