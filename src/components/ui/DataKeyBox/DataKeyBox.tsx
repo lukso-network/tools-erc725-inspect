db73f7e2061645b14a721aea0766cefdd6d05da3
CJ42 <cavallerajean@gmail.com>
2025-11-21 12:52:21 +0000

    refactor: adjust layout for collapsible data keys

--- src/components/ui/DataKeyBox/DataKeyBox.tsx
+++ src/components/ui/DataKeyBox/DataKeyBox.tsx
@@ -25,79 +25,81 @@ const DataKeyBox = ({ address, data }: DataKeyBoxProps) => {
   const columnClass = keyType === 'Array' ? 'is-full' : 'is-two-thirds';
 
   return (
-    <div className="columns is-multiline my-3 dataKeyBox" key={data.key}>
-      <div className={`column ${columnClass}`}>
-        <div className="content">
-          <details className={`${styles.dataKeyBoxDetails} is-open`} open>
-            <summary className="has-text-weight-semibold is-clickable">
-              <div className="title is-4">
-                {data.schema.name in SchemaName ? (
-                  <a
-                    href={SCHEMA_DOCS_LINKS[data.schema.name]}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="home-link"
-                  >
-                    {data.schema.name} ↗️
-                  </a>
-                ) : (
-                  data.schema.name
-                )}
+    <details
+      className={`${styles.dataKeyBox} content my-3 p-3 is-open`}
+      key={data.key}
+      open
+    >
+      <summary className="has-text-weight-semibold is-clickable">
+        <div className="title is-4">
+          {data.schema.name in SchemaName ? (
+            <a
+              href={SCHEMA_DOCS_LINKS[data.schema.name]}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="home-link"
+            >
+              {data.schema.name} ↗️
+            </a>
+          ) : (
+            data.schema.name
+          )}
 
-                <span className="tag is-small mx-2 is-info">
-                  {data.schema.keyType}
-                </span>
-              </div>
-            </summary>
-            <CollapsibleSchema schema={data.schema} />
-            <ul>
+          <span className="tag is-small mx-2 is-info">
+            {data.schema.keyType}
+          </span>
+        </div>
+      </summary>
+      <div className="columns is-multiline">
+        <div className={`column ${columnClass}`}>
+          <CollapsibleSchema schema={data.schema} />
+          <ul>
+            <li>
+              <strong>Key:</strong> <code>{data.schema.key}</code>
+            </li>
+            <li>
+              <strong>Raw value: </strong>
+              <span className="tag is-small mx-2 is-link is-light">
+                {data.schema.valueType}
+              </span>
+              <code>{data.value}</code>
+            </li>
+            <li>
+              <strong>Value Content: </strong>
+              <span className="tag is-small is-link is-light">
+                {data.schema.valueContent}
+              </span>
+            </li>
+            <li>
+              <strong>Decoded value: </strong>
+              <span className="my-3 mr-3">
+                <ValueTypeDecoder
+                  address={address}
+                  erc725JSONSchema={data.schema}
+                  value={data.value}
+                />
+              </span>
+            </li>
+            {data.schema.keyType === 'MappingWithGrouping' && (
               <li>
-                <strong>Key:</strong> <code>{data.schema.key}</code>
+                Mapped address:{' '}
+                <code>0x{data.schema.name.split(':').pop()}</code>{' '}
               </li>
-              <li>
-                <strong>Raw value: </strong>
-                <span className="tag is-small mx-2 is-link is-light">
-                  {data.schema.valueType}
-                </span>
-                <code>{data.value}</code>
-              </li>
-              <li>
-                <strong>Value Content: </strong>
-                <span className="tag is-small is-link is-light">
-                  {data.schema.valueContent}
-                </span>
-              </li>
-              <li>
-                <strong>Decoded value: </strong>
-                <span className="my-3 mr-3">
-                  <ValueTypeDecoder
-                    address={address}
-                    erc725JSONSchema={data.schema}
-                    value={data.value}
-                  />
-                </span>
-              </li>
-              {data.schema.keyType === 'MappingWithGrouping' && (
-                <li>
-                  Mapped address:{' '}
-                  <code>0x{data.schema.name.split(':').pop()}</code>{' '}
-                </li>
-              )}
-            </ul>
-          </details>
+            )}
+          </ul>
+        </div>
+        <div className="column">
+          {keyType !== 'Array' &&
+            value !== '0x' &&
+            valueContent === 'Address' && (
+              <AddressButtons
+                showInspectButton={true}
+                address={value as string}
+              />
+            )}
         </div>
       </div>
-      <div className="column">
-        {keyType !== 'Array' &&
-          value !== '0x' &&
-          valueContent === 'Address' && (
-            <AddressButtons
-              showInspectButton={true}
-              address={value as string}
-            />
-          )}
-      </div>
-    </div>
+    </details>
   );
 };
 
